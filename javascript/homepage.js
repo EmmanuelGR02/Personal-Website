a1320d787d5c494c7142024fdb69fae0b19270d2
Emmanuel <e.gutierrezrivera02@gmail.com>
2024-03-06 21:24:05 -0500

    styling

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -4,7 +4,7 @@
 document.addEventListener('mousemove', function(event) {
     const body = document.querySelector('body');
     const mouseX = Math.round((event.clientX / window.innerWidth)); // Adjusted to fit within 0-2 range
-    const colors = ['gray', 'black']; // Array of colors (gray, black, white)
+    const colors = ['black', 'gray']; // Array of colors (gray, black, white)
     body.style.backgroundColor = colors[mouseX];
 });
 
