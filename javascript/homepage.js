625ab69fcc715176c0cb4bc2d592628bcfdb05d0
Emmanuel <e.gutierrezrivera02@gmail.com>
2024-03-07 21:24:01 -0500

    added personal pic and hover animation to header text

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,10 +1,10 @@
 
 
-// Change background color between gray, black, and white based on mouse position
-document.addEventListener('mousemove', function(event) {
-    const body = document.querySelector('body');
-    const mouseX = Math.round((event.clientX / window.innerWidth)); // Adjusted to fit within 0-2 range
-    const colors = ['black', 'gray']; // Array of colors (gray, black, white)
-    body.style.backgroundColor = colors[mouseX];
-});
+function change_background_black() {
+    body.style.change_backgroundColor = 'black'
+}
+function change_background_gray() {
+    body.style.change_backgroundColor = 'gray'
+}
+
 
