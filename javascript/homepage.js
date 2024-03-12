7666d6eddb9d7b6545887d666d67cf2700c7b8af
Emmanuel <e.gutierrezrivera02@gmail.com>
2024-03-12 10:38:39 -0400

    added firebase database

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -36,5 +36,25 @@ backgroundSwitch.addEventListener('change', function() {
 
 
 
+// ************************** FIREBASE **************************
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5t42GhysCIOJBGQ0IoHftfe3481h9oLA",
+  authDomain: "personal-website-6c1eb.firebaseapp.com",
+  projectId: "personal-website-6c1eb",
+  storageBucket: "personal-website-6c1eb.appspot.com",
+  messagingSenderId: "73260029859",
+  appId: "1:73260029859:web:a3df6e70e6a4ca0b1cec16",
+  measurementId: "G-DB74KTDP29"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
