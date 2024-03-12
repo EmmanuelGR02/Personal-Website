7524486a117dd4903594731deb81da5ec0682334
Emmanuel <e.gutierrezrivera02@gmail.com>
2024-03-12 11:41:01 -0400

    gitignore

---
+++ .gitignore/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyB5t42GhysCIOJBGQ0IoHftfe3481h9oLA",
+    authDomain: "personal-website-6c1eb.firebaseapp.com",
+    projectId: "personal-website-6c1eb",
+    storageBucket: "personal-website-6c1eb.appspot.com",
+    messagingSenderId: "73260029859",
+    appId: "1:73260029859:web:a3df6e70e6a4ca0b1cec16",
+    measurementId: "G-DB74KTDP29"
+};
+
+export default firebaseConfig;
