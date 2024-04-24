0b8167ab740b638dfed1c16766d0aae9bca27eda
Emmanuel <e.gutierrezrivera02@gmail.com>
2024-04-24 15:38:47 -0400

    styling and contact form is now successfully saving all the info in the database

--- javascript/about.js
+++ javascript/about.js
@@ -1,6 +1,8 @@
+
 document.addEventListener("DOMContentLoaded", function() {
     var skillsTab = document.getElementById("skills_tab");
     var skillsContainer = document.getElementById("skills_container");
+    var tab_text = document.getElementById("skills_tab");
 
     // Initially hide the skills container
     skillsContainer.style.display = "none";
@@ -9,8 +11,15 @@ document.addEventListener("DOMContentLoaded", function() {
     skillsTab.addEventListener("click", function() {
         if (skillsContainer.style.display === "none") {
             skillsContainer.style.display = "block";
+            tab_text.style.color = "black";
+            skillsTab.style.background = "white";
+            skillsTab.style.border = "2px solid black"
         } else {
             skillsContainer.style.display = "none";
+            tab_text.style.color = "white";
+            skillsTab.style.background = "black";
         }
     });
 });
+
+
