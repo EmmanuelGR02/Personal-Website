8a6fc219b8008c728b0b1ad8c675b2fcda0f0462
Emmanuel <e.gutierrezrivera02@gmail.com>
2025-01-19 01:49:52 -0500

    Update build config and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,17 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './javascript/contact.js',  // Your entry file (adjust if needed)
+    entry: './javascript/contact.js', 
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
-        extensions: ['.js', '.json'],
+        extensions: ['.js', '.json'], 
     },
     plugins: [
-        new Dotenv(),  // Load environment variables from .env
-        new HtmlWebpackPlugin({
-            template: './index.html',  // Path to your HTML template file
-        }),
+        new Dotenv() // Load environment variables from .env 
     ],
-    mode: 'development',  // Change to 'production' for production build
+    mode: 'development', 
 };
