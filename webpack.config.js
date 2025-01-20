6b78b0a8cacba906993a638deeec3b66f1c4c2c2
Emmanuel <e.gutierrezrivera02@gmail.com>
2025-01-20 14:29:24 -0500

    fixing issue with firebase not recieving data

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
-const Dotenv = require('dotenv-webpack'); // Correct import
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    entry: './javascript/homepage.js', // Main entry point
+    entry: './javascript/homepage.js',
     output: {
-        filename: 'bundle.js', // Output JS file without hashing
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true, // Clean the dist folder before building
-    },
-    resolve: {
-        extensions: ['.js', '.json'],
+        clean: true,
     },
     module: {
         rules: [
             {
-                test: /\.(png|jpe?g|gif|svg)$/i, // Handle image files
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name][ext]', // Output to the images folder
-                },
+                test: /\.html$/,
+                use: ['html-loader'],
             },
             {
-                test: /\.html$/, // Process and copy HTML files
-                use: ['html-loader'],
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
             },
         ],
     },
     plugins: [
-        new Dotenv(), // Correct use of dotenv-webpack
-        // Process the main index.html
         new HtmlWebpackPlugin({
-            template: './index.html', // Use your index.html
+            template: './index.html',
             filename: 'index.html',
-            inject: false, // Prevent injecting hashed files
         }),
-        // Copy static files (CSS, images, additional HTML files)
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'style', to: 'style' }, // Copy CSS files
-                { from: 'images', to: 'images' }, // Copy images
-                { from: 'javascript', to: 'javascript' }, // Copy JavaScript folder
-                { from: '*.html', to: '[name][ext]', globOptions: { ignore: ['**/index.html'] } }, // Exclude index.html
-            ],
-        }),
+        new Dotenv(), // This will replace `process.env` references
     ],
-    mode: 'production', // Use production mode
+    mode: 'production',
 };
